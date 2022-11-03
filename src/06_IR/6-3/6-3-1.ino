97f0edef2b923cc582e243f591f53d6a79a92f38
Juhong <55423899+juhong-rdv@users.noreply.github.com>
2022-11-03 10:25:24 +0900

    Add files via upload

---
+++ src/06_IR/6-3/6-3-1.ino
@@ -0,0 +1,27 @@
+int count = 0 ;             //카드가 몇번 꽂혔는지 count하는 변수
+int pulse_status = 0 ;      //카드의 상태를 잠시 저장하는 변수
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600) ;      //시리얼 통신 시작
+  pinMode(5, INPUT) ;       //포토인터럽트 센서의 Digital Output -5
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if( digitalRead(5) == HIGH )  //포토인터럽트에 장애물이 없는 경우
+  {
+    if( pulse_status == 1 )     //pulse_status : 0 - 직전에 카드가 존재 했었다, 1  - 직전에 카드가 존재하지 않았다.
+    {
+      count++ ;                 //카드가 꽂힌 수 1 증가
+      pulse_status = 0 ;        //포토인터럽트에 장애물이 없어서 반복 수행되더라도 cunt가 1만 증가 되도록 0으로 만든다.      
+    }
+  }
+  else                          //포토인터럽트에 장애물이 있는경우
+  {
+    pulse_status = 1 ;          //카드가 존재한다. pulse_status = 1 ;
+  }
+
+  Serial.print("count = ") ;    //카운트 출력
+  Serial.println(count) ;
+}
