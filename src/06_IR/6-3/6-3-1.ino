f50f6fd01ca6a08088691ded79a6d7f2277c2b3e
Juhong <55423899+juhong-rdv@users.noreply.github.com>
2022-11-03 11:50:47 +0900

    Add files via upload

--- src/06_IR/6-3/6-3-1.ino
+++ src/06_IR/6-3/6-3-1.ino
@@ -4,12 +4,12 @@ int pulse_status = 0 ;      //카드의 상태를 잠시 저장하는 변수
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600) ;      //시리얼 통신 시작
-  pinMode(5, INPUT) ;       //포토인터럽트 센서의 Digital Output -5
+  pinMode(5, INPUT_PULLUP) ;       //포토인터럽트 센서의 Digital Output -5
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if( digitalRead(5) == HIGH )  //포토인터럽트에 장애물이 없는 경우
+  if( digitalRead(5) == LOW )  //포토인터럽트에 장애물이 없는 경우
   {
     if( pulse_status == 1 )     //pulse_status : 0 - 직전에 카드가 존재 했었다, 1  - 직전에 카드가 존재하지 않았다.
     {
