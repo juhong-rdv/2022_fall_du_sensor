95c09cc146612147f06010ccaf8c262666c0dbcc
Juhong <55423899+juhong-rdv@users.noreply.github.com>
2022-11-23 18:47:47 +0900

    Add files via upload

---
+++ src/09_opencv/08_roi_image.cpp
@@ -0,0 +1,22 @@
+#include "opencv2/opencv.hpp"	
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World\n") ;
+	cv::Mat image = cv::imread("/home/dsu/test/test2.jpg") ;
+
+	cv::Rect roi ;
+	roi.x = 177 ;
+	roi.y = 230 ;
+	roi.width = 125 ;
+	roi.height = 100 ;
+	
+	cv::Mat roi_image = image(roi) ;
+	 
+	cv::imshow("test", image) ;
+	cv::imshow("roi", roi_image) ;
+	cv::waitKey(0) ;
+	
+	return 0 ;
+}
